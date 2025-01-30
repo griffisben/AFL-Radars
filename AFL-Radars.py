5bf59c7416f5d63eef002f3eb4e976f2c1260141
Ben Griffis <griffis.ben16@gmail.com>
2025-01-30 13:44:59 -0500

    Update AFL-Radars.py

--- AFL-Radars.py
+++ AFL-Radars.py
@@ -459,7 +459,7 @@ def scout_report(league, season, pos, mins, name,callout, bar_colors, dist_label
 ########################################################################
 
 st.title("Footy Radars :rugby_football:")
-st.header("All data via AFL")
+st.subheader("All data via AFL")
 
 avail_data = pd.read_csv(f"https://raw.githubusercontent.com/griffisben/AFL-Radars/refs/heads/main/AvailableData.csv")
 
