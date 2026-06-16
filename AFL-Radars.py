4d0ba9f26713015ce48ddb7ee5a21ea7f26d7bda
Ben Griffis <griffis.ben16@gmail.com>
2026-06-16 13:37:06 +0200

    Update AFL-Radars.py

--- AFL-Radars.py
+++ AFL-Radars.py
@@ -629,8 +629,8 @@ def scout_report(league, season, pos, mins, name,callout, bar_colors, dist_label
     newax.imshow(image)
     newax.axis('off')
     
-    response = requests.get(team_pic)
-    image = Image.open(response.raw)
+    urllib.request.urlretrieve(team_pic,'team_pic.png')
+    image = Image.open('team_pic.png')
     newax = fig.add_axes([.15,.82,0.1,0.1], anchor='C', zorder=1)
     newax.imshow(image)
     newax.axis('off')
